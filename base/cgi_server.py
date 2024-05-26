0c7beebab8d57494013789a7cf426cfedfa4b45c
KoschavtcevDaniel <112947715+KoschavtcevDaniel@users.noreply.github.com>
2024-05-26 22:50:52 +0300

    Delete base directory

--- base/cgi_server.py
+++
@@ -1,6 +0,0 @@
-from http.server import HTTPServer
-from http.server import CGIHTTPRequestHandler
-
-server_address = ('localhost', 8888)
-http_server = HTTPServer(server_address, CGIHTTPRequestHandler)
-http_server.serve_forever()
